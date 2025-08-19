c2441b3dcb596230fe9a0309205e7d6d3c7a2575
Hveppe <160583356+Hveppe@users.noreply.github.com>
2025-08-19 13:25:57 +0200

    Update main.ino with semi working letter change

--- main.ino
+++ main.ino
@@ -1,7 +1,15 @@
 const int buttonPin = 5;
-unsigned long start = millis();
+const int longThreshold = 200;
+const int newLetterTime = 500;
+const int newWordTime;
+
+unsigned long start = 0;
 unsigned long end = 0;
+unsigned long currentTime = millis();
 unsigned long elapsedTime = 0;
+
+int turn = 0;
+int letter[5] = {0, 0, 0, 0, 0}; // 0 er ingenting, 1 er en prik og 2 er en streng
 
 bool isPressed = false;
 
@@ -12,6 +20,7 @@ void setup() {
 
 void loop() {
   int buttonState = digitalRead(buttonPin);
+  currentTime = millis();
 
   if(buttonState == 0 && !isPressed) {
     start = millis();
@@ -21,11 +30,21 @@ void loop() {
     end = millis();
     elapsedTime = end - start;
     
-    if(elapsedTime > 10) {
-      Serial.println(elapsedTime);
+    if(elapsedTime > 10 && turn < 5) {
+      if(elapsedTime < longThreshold) {
+        letter[turn] = 1;
+      } else {
+        letter[turn] = 2;
+      }
+
+      turn += 1;
     }
 
     isPressed = false;
   }
+
+  if(newLetterTime < (currentTime - end) && buttonState == 1) {
+      turn = 0;
+    }
 
 }
